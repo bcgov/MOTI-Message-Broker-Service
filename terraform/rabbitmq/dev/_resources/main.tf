d4359c91ff6a3c325f94b05b259ac5a6c684e5d5
j.dev <junmin.dev@outlook.com>
2024-10-10 08:45:32 -0700

    chore: add sample terraform scripts

---
+++ terraform/rabbitmq/dev/_resources/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    rabbitmq = {
+      source  = "cyrilgdn/rabbitmq"
+      version = "=1.8.0"
+    }
+  }
+}
+
+resource "rabbitmq_vhost" "sawsx" {
+  name = "sawsx"
+}
